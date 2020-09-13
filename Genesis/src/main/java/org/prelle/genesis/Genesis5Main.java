38a4a6113a05a243f56becd52b1e2f818894205e
Stefan Prelle <stefan@rpgframework.de>
2020-09-13 21:47:55 +0200

    Fix MAC OS detection

--- Genesis/src/main/java/org/prelle/genesis/Genesis5Main.java
+++ Genesis/src/main/java/org/prelle/genesis/Genesis5Main.java
@@ -153,7 +153,7 @@ public class Genesis5Main extends Application {
 	//-------------------------------------------------------------------
 	private Path getUserInstallationDirectory() {
 		Path home = Paths.get(System.getProperty("user.home"));
-		if (System.getProperty("os.name").toLowerCase().indexOf("mac")>0) {
+		if (System.getProperty("os.name").toLowerCase().indexOf("mac")>=0) {
 			// Running on Mac OS
 			return home.resolve("Library").resolve("Application Support").resolve("de.rpgframework.Genesis").resolve(System.getProperty("profile","no-profile"));
 		} else
