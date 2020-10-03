7c596819e8ed500c63f46737b854067eb8f18567
Stefan Prelle <stefan@prelle.org>
2020-10-03 10:40:28 +0200

    Includes SR now

--- Genesis/src/main/java/org/prelle/genesis/print/PDFPrintElementListCell.java
+++ Genesis/src/main/java/org/prelle/genesis/print/PDFPrintElementListCell.java
@@ -1,12 +1,17 @@
 package org.prelle.genesis.print;
 
 import java.io.ByteArrayInputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import de.rpgframework.ResourceI18N;
 import de.rpgframework.print.PDFPrintElement;
 import de.rpgframework.print.PDFPrintElement.RenderingParameter;
+import de.rpgframework.print.PDFPrintElementFeature;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.SnapshotParameters;
@@ -24,6 +29,7 @@ import javafx.scene.layout.VBox;
 class PDFPrintElementListCell extends ListCell<PDFPrintElement> {
 
 	private final static Logger logger = LogManager.getLogger("genesis");
+	private final static ResourceBundle RES = ResourceBundle.getBundle(PDFPrintElementListCell.class.getName());
 
 	private PDFPrintElement data;
 
@@ -53,7 +59,29 @@ class PDFPrintElementListCell extends ListCell<PDFPrintElement> {
 			VBox imgBox = new VBox(iview);
 			imgBox.setPadding(new Insets(2));
 
-			Label label = new Label(item.getDescription());
+			// Build a list of features
+			List<String> features = new ArrayList<String>();
+			for (PDFPrintElementFeature feat : PDFPrintElementFeature.values()) {
+				if (item.hasFeature(feat)) {
+					switch (feat) {
+					case EXPAND_HORIZONTAL:
+						features.add(ResourceI18N.get(RES, "feature.horizontal")); break;
+					case EXPAND_VERTICAL:
+						features.add(ResourceI18N.get(RES, "feature.vertical")); break;
+					case FILTER:
+						features.add(ResourceI18N.get(RES, "feature.filter")); break;
+					case INDEXABLE:
+						features.add(ResourceI18N.get(RES, "feature.index")); break;
+					case ORIENTATION:
+						features.add(ResourceI18N.get(RES, "feature.orientation")); break;
+					}
+				}
+			}
+			String featText = "";
+			if (!features.isEmpty())
+				featText = " ("+String.join(",", features)+")";
+			
+			Label label = new Label(item.getDescription()+featText);
 //			label.setPrefWidth(Double.MAX_VALUE);
 			label.setStyle("-fx-background-color: rgba(0,0,0,0.5); -fx-text-fill: white; -fx-min-width: 25em");
 
